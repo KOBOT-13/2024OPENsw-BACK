ef3a2889da4002f88d8b0770fefff6c3eeb671be
forestsol <forestsol1214@gmail.com>
2024-07-09 11:57:15 +0900

    fixed untracked files

--- page/models.py
+++ page/models.py
@@ -1,3 +1,4 @@
 from django.db import models
 
-# Create your models here.
+class Video(models.Model):
+    custom_id = models.CharField(max_length=12, primary_key=True, editable=False, unique=True)

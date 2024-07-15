e1b24638f9cffc1d8bc5e591770ae2069c485fbd
forestsol <forestsol1214@gmail.com>
2024-07-15 12:50:34 +0900

    feat: 기능별 앱 추가.
    feat: 기능별 앱 추가.

---
+++ quizzes/urls.py
@@ -0,0 +1,15 @@
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
+from .views import *
+
+router = DefaultRouter()
+# DefaultRouter는 RESTful API의 라우팅을 자동으로 처리해주는 편리한 도구입니다
+# ex) router.register('pages', PageViewSet, basename = 'PageViewSet')
+
+
+urlpatterns = [
+    path('', include(router.urls)),
+    # ex) path('pages/<int:year>/', AllPagesView.as_view(), name='all-pages-list'),
+
+
+]

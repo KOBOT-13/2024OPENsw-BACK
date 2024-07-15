4611ac3f37ba555467fa1017968c7ba16a548492
forestsol <forestsol1214@gmail.com>
2024-07-15 13:54:56 +0900

    Test: admin페이지에 모델이 보이도록 하는 코드 예시.
    Test: admin페이지에 모델이 보이도록 하는 코드 예시.

--- ossKobot/settings.py
+++ ossKobot/settings.py
@@ -126,7 +126,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': [
-        'rest_framework.authentication.TokenAuthentication',
+        'rest_framework_simplejwt.authentication.JWTAuthentication',
     ],
     'DEFAULT_PERMISSION_CLASSES': [
         'rest_framework.permissions.IsAuthenticated',
--- pages/models.py
+++ pages/models.py
@@ -1,2 +1,7 @@
 from django.db import models
 
+class Page(models.Model):
+    date = models.DateField(auto_now_add=True, verbose_name="date")
+
+    def __str__(self):
+        return self.date

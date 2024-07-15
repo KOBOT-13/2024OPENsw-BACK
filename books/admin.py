9cb1703f9106d51f326301ab6862f68a9f0f6443
HYEJIN <imagine@kookmin.ac.kr>
2024-07-15 13:32:44 +0900

    Refactor: quizzes 앱으로 관련 admin설정 이동

--- books/admin.py
+++ books/admin.py
@@ -1,7 +1,5 @@
 from django.contrib import admin
-from .models import Book, Character, Quiz, Question
+from .models import Book, Character
 
 admin.site.register(Book)
 admin.site.register(Character)
-admin.site.register(Quiz)
-admin.site.register(Question)

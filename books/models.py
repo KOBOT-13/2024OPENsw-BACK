ad08a62c5ee954d402144e22abaf69c38112de25
HYEJIN <imagine@kookmin.ac.kr>
2024-07-15 13:16:13 +0900

    Rename: books 앱 내로 모델 이동
    앱이 추가되어 디렉토리 구조가 변경됨에 따라 page/models.py에 작성된 코드를 books/models.py로 옮깁니다.

--- books/models.py
+++ books/models.py
@@ -1,3 +1,53 @@
 from django.db import models
 
-# Create your models here.
+class Book(models.Model):
+    book_id = models.AutoField(primary_key=True) # id
+
+    title = models.CharField(max_length=200)  # 책 제목
+    author = models.CharField(max_length=100)  # 저자
+    publisher = models.CharField(max_length=200, blank=True, null=True) # 출판사
+    publication_date = models.DateField(blank=True, null=True) # 출판일자
+    cover_image = models.ImageField(upload_to='book_covers/', blank=True, null=True)  # 책 사진
+    synopsis = models.TextField(blank=True)  # 줄거리
+    content = models.TextField(blank=True) # 책 내용
+
+    created_at = models.DateTimeField(auto_now_add=True) # 책 추가 날짜
+    updated_at = models.DateTimeField(auto_now=True) # 책 수정 날짜
+
+    def __str__(self):
+        return self.title
+
+class Character(models.Model):
+    character_id = models.AutoField(primary_key=True) # id
+    name = models.CharField(max_length=100)  # 등장인물 이름
+    description = models.TextField(blank=True)  # 등장인물 설명
+    character_image = models.ImageField(upload_to='character_image/', blank=True, null=True)  # 등장인물 사진
+    book_id = models.ForeignKey(Book, related_name='characters', on_delete=models.CASCADE)  # 책과의 관계
+
+    def __str__(self):
+        return self.name
+
+class Quiz(models.Model):
+    quiz_id = models.AutoField(primary_key=True) # id
+
+    book = models.ForeignKey(Book, related_name='quizz', on_delete=models.CASCADE)  # 책과의 관계
+    title = models.CharField(max_length=200)  # 퀴즈 제목
+    quiz_image = models.ImageField(upload_to='quiz_images/', blank=True, null=True)  # 퀴즈 이미지
+
+    def __str__(self):
+        return self.title
+
+class Question(models.Model):
+    quiz_id = models.ForeignKey(Quiz, related_name='questions', on_delete=models.CASCADE)  # 퀴즈와의 관계
+
+    question_text = models.TextField()  # 질문 텍스트
+    correct_answer = models.IntegerField()  # 정답 (선지번호)
+    option_one = models.CharField(max_length=200)  # 선택지 1
+    option_two = models.CharField(max_length=200)  # 선택지 2
+    option_three = models.CharField(max_length=200, blank=True)  # 선택지 3
+    option_four = models.CharField(max_length=200, blank=True)  # 선택지 4
+    question_image = models.ImageField(upload_to='question_images/', blank=True, null=True)  # 질문 이미지
+
+    def __str__(self):
+        return self.question_text
+

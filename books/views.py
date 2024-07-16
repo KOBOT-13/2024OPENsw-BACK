4dcafefcf3d8269b809e96bc9b0e8cab86e0369a
HYEJIN <imagine@kookmin.ac.kr>
2024-07-16 10:54:40 +0900

    Feat: 책 별 등장인물 검색 API View 추가

--- books/views.py
+++ books/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 from rest_framework.views import APIView
 from rest_framework.response import Response
-from rest_framework import status, viewsets
+from rest_framework import status, viewsets, generics
 from .serializers import *
 
 # Create your views here.
@@ -13,3 +13,10 @@ class BookViewSet(viewsets.ModelViewSet):
 class CharacterViewSet(viewsets.ModelViewSet):
     queryset = Character.objects.all()
     serializer_class = CharacterSerializer
+
+class BookCharactersAPIView(generics.ListAPIView):
+    serializer_class = CharacterSerializer
+
+    def get_queryset(self):
+        book_id = self.kwargs['book_id']
+        return Character.objects.filter(book_id=book_id)

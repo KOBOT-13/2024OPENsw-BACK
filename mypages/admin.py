126db7927c922c6fa4473fa60cfe1699c0f62b9b
HYEJIN <imagine@kookmin.ac.kr>
2024-07-18 15:24:10 +0900

    Feat: 퀴즈기록, 대화 기록 admin 등록

--- mypages/admin.py
+++ mypages/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
+from .models import QuizRecord, ConversationRecord
 
-# Register your models here.
+admin.site.register(QuizRecord)
+admin.site.register(ConversationRecord)

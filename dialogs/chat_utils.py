64e0831da5ba6d1e11cf6fcb760d90fc61c59459
OnlyWonHand <113083948+IamWonILuvWon@users.noreply.github.com>
2024-07-18 14:11:56 +0900

    feat: character key 값을 받아서 등장인물 설정하는 코드 추가

--- dialogs/chat_utils.py
+++ dialogs/chat_utils.py
@@ -1,22 +1,30 @@
 import os
 from openai import OpenAI
 
-# 발급받은 API 키 설정
-api_key = "user_API_KEY" #API 키는 원준이에게 여쭤보세요..!
+api_key = "API_KEY"
 os.environ["OPENAI_API_KEY"] = api_key
 
-# OpenAI 클라이언트 설정
-
 client = OpenAI()
+character_key = 1
 
-# 키 값을 받고 if 문으로 로직 짜주세요
-characters = "답변은 한국어로 하고 백설공주의 마녀의 입장에서 대답해줘"
+character_map = {
+    0: "답변은 한국어로 하고 백설공주의 마녀의 입장에서 대답해줘",
+    1: "답변은 한국어로 하고 토끼와 거북이의 토끼의 입장에서 대답해줘",
+    2: "답변은 한국어로 하고 토끼와 거북이의 거북이의 입장에서 대답해줘",
+    3: "답변은 한국어로 하고 아기돼지 삼형제의 첫째돼지의 입장에서 대답해줘",
+    4: "답변은 한국어로 하고 아기돼지 삼형제의 둘째돼지의 입장에서 대답해줘",
+    5: "답변은 한국어로 하고 아기돼지 삼형제의 셋째돼지의 입장에서 대답해줘"
+}
 
+# character_key에 해당하는 characters를 설정
+characters = character_map[character_key]
+
+    
 # 챗봇 함수 정의
 
 def chatbot(input_message):
     try:
-        # GPT-4 모델 선택
+        
         model = "gpt-3.5-turbo"
        
         messages = [
@@ -37,7 +45,7 @@ def chatbot(input_message):
             temperature=0
         )
 
-        # ChatCompletion 객체에서 응답 메시지 추출
+        # 응답 메시지 추출
         if response.choices:
             bot_response = response.choices[0].message.content
         else:
@@ -55,7 +63,7 @@ if __name__ == "__main__":
     while True:
         user_input = input("사용자: ")
 
-        # 종료 조건 설정
+        # 종료 조건 설정 --> (프론트에서 창을 나가면 받는 값으로 설정해주면 될 듯 합니다.~)
         if user_input.lower() == '그만':
             print("챗봇을 종료합니다.")
             break

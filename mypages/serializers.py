637f9a1ab293c0218418c4889c0d501f3f9652c4
HYEJIN <imagine@kookmin.ac.kr>
2024-07-18 15:22:46 +0900

    Feat: mypages 앱 내 Serializers 설정

--- mypages/serializers.py
+++ mypages/serializers.py
@@ -1,1 +1,12 @@
 from rest_framework import serializers
+from .models import QuizRecord, ConversationRecord
+
+class QuizRecordSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = QuizRecord
+        fields = '__all__'
+
+class ConversationRecordSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = ConversationRecord
+        fields = '__all__'

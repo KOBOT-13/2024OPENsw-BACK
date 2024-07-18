5ab2c920aa9149897208dc69f5b35a8383c01f20
HYEJIN <imagine@kookmin.ac.kr>
2024-07-18 15:23:25 +0900

    Feat: mypages 앱 내 ViewSet 구현

--- mypages/views.py
+++ mypages/views.py
@@ -1,4 +1,11 @@
 from django.shortcuts import render
 from rest_framework import viewsets
+from .serializers import *
 
-# Create your views here.
+class QuizRecordViewSet(viewsets.ModelViewSet):
+    queryset = QuizRecord.objects.all()
+    serializer_class = QuizRecordSerializer
+
+class ConversationRecordViewSet(viewsets.ModelViewSet):
+    queryset = ConversationRecord.objects.all()
+    serializer_class = ConversationRecordSerializer

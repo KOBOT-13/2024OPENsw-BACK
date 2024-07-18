3cd720a89eb4b18546a02e08422202af8c7ff6c9
HYEJIN <imagine@kookmin.ac.kr>
2024-07-18 15:22:18 +0900

    Refactor: 모델 FK 참조 수정

--- mypages/models.py
+++ mypages/models.py
@@ -1,11 +1,10 @@
 from django.db import models
-from .models import User, Book, Quiz, Conversation
 from django.conf import settings  
 
 class QuizRecord(models.Model):
     id = models.AutoField(primary_key=True) # 퀴즈 기록 ID, 고유 식별자
-    quiz = models.ForeignKey(Quiz, on_delete=models.CASCADE) # 퀴즈 ID (참조: Quiz 테이블)
     user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE) # 사용자 ID (참조: User 테이블)
+    quiz = models.ForeignKey('quizzes.Quiz', on_delete=models.CASCADE) # 퀴즈 ID (참조: Quiz 테이블)
     score = models.IntegerField() # 퀴즈 점수
     completed_at = models.DateTimeField()  # 퀴즈를 완료한 날짜와 시간
 
@@ -14,8 +13,8 @@ class QuizRecord(models.Model):
 
 class ConversationRecord(models.Model):
     id = models.AutoField(primary_key=True) # 퀴즈 기록 ID, 고유 식별자
-    conversation = models.ForeignKey(Conversation, on_delete=models.CASCADE) # 대화 ID (참조: Conversation 테이블)
     user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE) # 사용자 ID (참조: User 테이블)
+    conversation = models.ForeignKey('dialogs.Conversation', on_delete=models.CASCADE) # 대화 ID (참조: Conversation 테이블)
     completed_at = models.DateTimeField()  # 대화를 완료한 날짜와 시간
 
     def __str__(self):

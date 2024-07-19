aad6718e0bc91756038dc6670c77f71c77e49372
HYEJIN <imagine@kookmin.ac.kr>
2024-07-19 15:23:05 +0900

    Chore: admin에 표시할 필드 명시

--- mypages/admin.py
+++ mypages/admin.py
@@ -1,5 +1,8 @@
 from django.contrib import admin
 from .models import QuizRecord, ConversationRecord
 
+class QuizRecordAdmin(admin.ModelAdmin):
+    list_display = ('id', 'user', 'quiz', 'score', 'completed_at')
+    
 admin.site.register(QuizRecord)
 admin.site.register(ConversationRecord)

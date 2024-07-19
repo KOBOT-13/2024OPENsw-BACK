1aeae195c4c85fb6343d34d022e1f5fc45679f35
OnlyWonHand <113083948+IamWonILuvWon@users.noreply.github.com>
2024-07-19 11:04:40 +0900

    feat: gitignore 추가와 채팅 요약 및 api숨기기 추가

--- dialogs/chat_utils.py
+++ dialogs/chat_utils.py
@@ -1,8 +1,20 @@
 import os
 from openai import OpenAI
+from langchain.memory import ConversationSummaryMemory
+from langchain_openai import ChatOpenAI
+from dotenv import load_dotenv
 
-api_key = "API_KEY"
+load_dotenv()
+
+api_key = os.getenv('api_key')
+
+chat_model = ChatOpenAI(openai_api_key=api_key)
+
 os.environ["OPENAI_API_KEY"] = api_key
+
+#대화 내용 요약 
+memory = ConversationSummaryMemory(
+    llm= ChatOpenAI(temperature = 0), return_messages=True)
 
 client = OpenAI()
 
@@ -18,12 +30,13 @@ character_map = {
     3: "답변은 한국어로 하고 아기돼지 삼형제의 첫째돼지의 입장에서 대답해줘",
     4: "답변은 한국어로 하고 아기돼지 삼형제의 둘째돼지의 입장에서 대답해줘",
     5: "답변은 한국어로 하고 아기돼지 삼형제의 셋째돼지의 입장에서 대답해줘"
-}
+}  # 겹치는 부분은 모델 자체에 넣기 
 
 # character_key에 해당하는 characters를 설정
 characters = character_map[character_key]
 
-    
+
+
 # 챗봇 함수 정의
 
 def chatbot(input_message):
@@ -32,6 +45,7 @@ def chatbot(input_message):
         model = "gpt-3.5-turbo"
        
         convo_history.append({"role": "user", "content": input_message})
+        
         
         messages = [
                 {   "role":"system",
@@ -57,7 +71,9 @@ def chatbot(input_message):
         else:
             bot_response = "No response from the model"
         convo_history.append({"role": "assistant", "content": bot_response})
-        
+        memory.save_context(
+            inputs= { "user" : input_message},
+            outputs={"assistant" : bot_response})
         return bot_response
 
     except Exception as e:
@@ -73,6 +89,9 @@ if __name__ == "__main__":
         # 종료 조건 설정 --> (프론트에서 창을 나가면 받는 값으로 설정해주면 될 듯 합니다.~)
         if user_input.lower() == '그만':
             print("챗봇을 종료합니다.")
+            
+            #대화 내용 요약 출력
+            print(memory.load_memory_variables({})["history"])
             break
 
         # 챗봇 함수 호출 및 응답 출력

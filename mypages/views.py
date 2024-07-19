f487fa2f8d08f81d34cf8480b1497ad76187552c
HYEJIN <imagine@kookmin.ac.kr>
2024-07-19 15:24:42 +0900

    Feat: `퀴즈 기록 목록 반환 API` 구현

--- mypages/views.py
+++ mypages/views.py
@@ -1,6 +1,12 @@
 from django.shortcuts import render
-from rest_framework import viewsets
+from rest_framework import viewsets, generics, status
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.views import APIView
+from rest_framework.response import Response
 from .serializers import *
+from .models import QuizRecord, ConversationRecord
+from quizzes.models import Quiz
+from django.utils import timezone
 
 class QuizRecordViewSet(viewsets.ModelViewSet):
     queryset = QuizRecord.objects.all()
@@ -9,3 +15,10 @@ class QuizRecordViewSet(viewsets.ModelViewSet):
 class ConversationRecordViewSet(viewsets.ModelViewSet):
     queryset = ConversationRecord.objects.all()
     serializer_class = ConversationRecordSerializer
+class QuizRecordListView(generics.ListAPIView):
+    serializer_class = QuizRecordSerializer
+    permission_classes = [IsAuthenticated]
+
+    def get_queryset(self):
+        user = self.request.user
+        return QuizRecord.objects.filter(user=user).select_related('quiz').order_by('-completed_at')

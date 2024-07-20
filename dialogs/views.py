70f261500f469a06958c24702fd15d3e546bf819
HYEJIN <imagine@kookmin.ac.kr>
2024-07-20 15:58:18 +0900

    Feat: 대화기록 조회 API 구현

--- dialogs/views.py
+++ dialogs/views.py
@@ -2,11 +2,18 @@ from django.shortcuts import render
 from rest_framework.views import APIView
 from rest_framework import status, viewsets, generics
 from .serializers import *
+from rest_framework.permissions import IsAuthenticated
 
 class ConversationViewSet(viewsets.ModelViewSet):
     queryset = Conversation.objects.all()
     serializer_class = ConversationSerializer
+    permission_classes = [IsAuthenticated]
+
+    def perform_create(self, serializer):
+        serializer.save(user=self.request.user)
+
 
 class MessageViewSet(viewsets.ModelViewSet):
     queryset = Message.objects.all()
     serializer_class = MessageSerializer
+    permission_classes = [IsAuthenticated]

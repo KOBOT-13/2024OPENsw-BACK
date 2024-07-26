dadd3fd52f498bf8c20849fafcaca740faec65d3
HYEJIN <imagine@kookmin.ac.kr>
2024-07-26 13:52:39 +0900

    Feat: `CustomConfirmEmailView` 구현

--- users/views.py
+++ users/views.py
@@ -4,6 +4,9 @@ from django.shortcuts import render
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework.permissions import IsAuthenticated
+from allauth.account.views import ConfirmEmailView
+from allauth.account.models import EmailConfirmationHMAC
+
 
 class ProfileView(APIView):
     permission_classes = [IsAuthenticated]
@@ -17,6 +20,38 @@ class ProfileView(APIView):
         })
 
 
+class CustomConfirmEmailView(ConfirmEmailView):
+    template_name = 'account/email/success_verify_email.html'
+    
+    def get_template_names(self) -> list[str]:
+        return super().get_template_names()
+    
+    def dispatch(self, request, *args, **kwargs):
+        key = kwargs.get('key', None)
+        print(f'dispatch called with key: {key}')
+        if key:
+            email_confirmation = EmailConfirmationHMAC.from_key(key)
+            if email_confirmation:
+                email_address = email_confirmation.email_address
+                print(f'EmailConfirmation found: {email_confirmation}') 
+                
+                # 여기서 이메일 인증을 완료
+                email_confirmation.confirm(request)
+                
+                # 다시 한 번 확인
+                email_address.refresh_from_db()
+                
+                if email_address and email_address.verified:
+                    user = email_address.user
+                    user.is_verified = True
+                    user.save()
+                    print(f'User {user.email} verified and saved')
+                else:
+                    print(f'EmailAddress not verified or not found: {email_address.email if email_address else "None"}')
+            else:
+                print(f'EmailConfirmation not found for key: {key}')
+        
+        return super().dispatch(request, *args, **kwargs)
 
 #카카오톡 로그인 뷰
 """from django.shortcuts import render

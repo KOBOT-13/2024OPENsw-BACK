88ec2a973eed965e44b8d65f74f2523b7c65f0ef
coladribble <134242170+coladribble@users.noreply.github.com>
2024-07-26 14:19:13 +0900

    Delete audios directory

--- audios/serializers.py
+++
@@ -1,9 +0,0 @@
-import os
-import json
-from rest_framework import serializers
-from .models import *
-
-class AudioTextSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = AudioText
-        fields = '__all__'

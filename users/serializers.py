1f7ef61311f95e3abd6037e6ea94de5a68ad8293
HYEJIN <imagine@kookmin.ac.kr>
2024-07-27 13:32:18 +0900

    Feat: 비밀번호 검증 함수 추가

--- users/serializers.py
+++ users/serializers.py
@@ -1,5 +1,6 @@
 from rest_framework import serializers
 from .models import CustomUser
+from django.contrib.auth.password_validation import validate_password
 
 class CustomUserSerializer(serializers.ModelSerializer):
     password1 = serializers.CharField(write_only=True)
@@ -8,6 +9,15 @@ class CustomUserSerializer(serializers.ModelSerializer):
     class Meta:
         model = CustomUser
         fields = ['username', 'email', 'password1', 'password2', 'birth_date']
+
+
+    def validate(self, data):
+        if data['password1'] != data['password2']:
+            raise serializers.ValidationError("Passwords do not match.")
+        
+        validate_password(data['password1'])
+        
+        return data
 
     def create(self, validated_data):
         if validated_data['password1'] != validated_data['password2']:

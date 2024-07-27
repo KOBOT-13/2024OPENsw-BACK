cd66d649d17ee285fabd0d3e06e03154eb3729ae
HYEJIN <imagine@kookmin.ac.kr>
2024-07-27 13:26:10 +0900

    Feat: `CustomUserSerializer` 구현

--- users/serializers.py
+++ users/serializers.py
@@ -1,2 +1,23 @@
 from rest_framework import serializers
+from .models import CustomUser
 
+class CustomUserSerializer(serializers.ModelSerializer):
+    password1 = serializers.CharField(write_only=True)
+    password2 = serializers.CharField(write_only=True)
+
+    class Meta:
+        model = CustomUser
+        fields = ['username', 'email', 'password1', 'password2', 'birth_date']
+
+    def create(self, validated_data):
+        if validated_data['password1'] != validated_data['password2']:
+            raise serializers.ValidationError({"password": "Passwords do not match."})
+
+        user = CustomUser(
+            username=validated_data['username'],
+            email=validated_data['email'],
+            birth_date=validated_data['birth_date'],
+        )
+        user.set_password(validated_data['password1'])
+        user.save()
+        return user

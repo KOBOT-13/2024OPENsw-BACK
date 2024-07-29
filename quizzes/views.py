3641795e7dae4b12e1e3347be8f8fd287f3750f8
forestsol <forestsol1214@gmail.com>
2024-07-29 22:58:34 +0900

    Feat: 책에 해당하는 퀴즈를 반환하는 api 구현
    Feat: 책에 해당하는 퀴즈를 반환하는 api 구현

--- quizzes/views.py
+++ quizzes/views.py
@@ -2,8 +2,15 @@ from django.shortcuts import render
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status, viewsets
+from rest_framework.generics import ListAPIView, RetrieveAPIView
+from rest_framework.permissions import IsAuthenticated
+from .models import Quiz
 from .serializers import *
 
-class QuizViewSet(viewsets.ModelViewSet):
-    queryset = Quiz.objects.all()
+class AllQuizzesByBookView(ListAPIView):
     serializer_class = QuizSerializer
+    permission_classes = [IsAuthenticated]
+    def get_queryset(self):
+        book_id = self.kwargs['book_id']
+        return Quiz.objects.filter(book_id=book_id).order_by('id')
+
